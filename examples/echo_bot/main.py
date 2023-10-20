f09d5316598f3f9f839025230abf042e82eb42bd
karelinaas <karelinaas95@gmail.com>
2023-10-20 23:31:23 +0300

    comments & some changes

--- examples/echo_bot/main.py
+++ examples/echo_bot/main.py
@@ -1,9 +1,9 @@
 # Из библиотеки aiogram импортируем классы Bot и Dispatcher
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 # Из aiogram.filters импортируем класс Command, чтобы фильтровать апдейты по наличию в них команд
 from aiogram.filters import Command
 # Из aiogram.types импортируем класс Message. Апдейты этого типа мы будем ловить эхо-ботом.
-from aiogram.types import Message
+from aiogram.types import ContentType, Message
 
 from config import *
 
@@ -21,6 +21,7 @@ async def process_start_command(message: Message):
 # Этот хэндлер будет срабатывать на команду "/help"
 @dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
+    # message.answer отправляет в чат сообщение, а message.reply отправляет ответ на сообщение
     await message.answer(
         'Напиши мне что-нибудь и в ответ '
         'я пришлю тебе твое сообщение'
@@ -29,9 +30,47 @@ async def process_help_command(message: Message):
 
 # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
 # кроме команд "/start" и "/help"
-@dp.message()
 async def send_echo(message: Message):
     await message.reply(text=message.text)
+
+
+# Этот хэндлер будет срабатывать на любые ваши сообщения,
+# кроме команд "/start" и "/help". ЕСЛИ его зарегистрировать!!!
+async def send_echo_extended(message: Message):
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(
+            text='Данный тип апдейтов не поддерживается '
+                 'методом send_copy'
+        )
+
+
+# Этот хэндлер будет срабатывать на отправку боту фото
+async def send_photo_echo(message: Message):
+    print(message)
+    await message.reply_photo(message.photo[0].file_id)
+
+    # Для аудио используется message.answer_audio(...)
+    # Для видео - message.answer_video(...)
+    # Для стикеров message.answer_sticker(...)
+
+
+async def send_sticker_echo(message: Message):
+    print(message)
+    await message.reply_sticker(message.sticker.file_id)
+
+
+# Альтернативный вариант регистрации хэндлеров (вместо декораторов)
+dp.message.register(send_photo_echo, F.content_type == ContentType.PHOTO)
+# Можно проще:
+# dp.message.register(send_photo_echo, F.photo)
+dp.message.register(send_sticker_echo, F.sticker)
+
+# dp.message.register(process_start_command, Command(commands='start'))
+# dp.message.register(process_help_command, Command(commands='help'))
+# send_echo по логике должен находиться в самом конце списка, т.к. он перехватывает любые сообщения
+dp.message.register(send_echo)
 
 
 if __name__ == '__main__':
